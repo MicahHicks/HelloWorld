6a0bba7d376206e9d1966dcefe033967e2f9bec9
Keagan Peterson <Keagan.a.peterson@outlook.com>
2023-03-25 17:29:05 -0500

    .

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ print('Hi ' + name)
 fav_color = input('What is your favorite  color? ')
 print(name + ' likes ' + fav_color)
 price = 3
-print('I am selling paint and i have your ' + fav_color + ' for ' + str(price) ' dollars')
-print('Would you like to purchase paint for ' + str(price) '? ')
+print('I am selling paint and i have your ' + fav_color + ' for ' + str(price) + ' dollars')
+print('Would you like to purchase paint for ' + str(price) + '? ')
