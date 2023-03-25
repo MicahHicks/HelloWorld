9922c9ff4fcdf2e39441f65553d9f2cd79a99f48
MicahHicks <hicamhicks@gmail.com>
2023-03-25 17:24:11 -0500

    .

---
+++ app.py
@@ -0,0 +1,7 @@
+name = input('What is your name? ')
+print('Hi ' + name)
+fav_color = input('What is your favorite  color? ')
+print(name + ' likes ' + fav_color)
+price = 3
+print('I am selling paint and i have your ' + fav_color + ' for ' + str(price) ' dollars')
+print('Would you like to purchase paint for ' + str(price) '? ')
